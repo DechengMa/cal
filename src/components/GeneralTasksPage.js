e82fa8e1a7d350e0c2415ecc3c374008ef7a3d43
damon yan <stylekn@hotmail.com>
2019-04-12 13:50:08 +1000

    update the ant-design

--- src/components/GeneralTasksPage.js
+++ src/components/GeneralTasksPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import {Tabs} from 'antd';
 import Tasks from '../elements/Tasks'
-
+const TabPane = Tabs.TabPane;
 class GenearlTasks extends React.Component{
     constructor(props){
         super(props);
@@ -12,8 +13,12 @@ class GenearlTasks extends React.Component{
     render(){
         return (
             <div>
-                <h2>Genearl Tasks</h2>
-                <Tasks />
+            <h2>Genearl Tasks</h2>
+            <Tabs defaultActiveKey="1">
+                <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+                <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+                <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+            </Tabs>
             </div>)
     }
 }
