604b31ac45ff12da2abed6b15fe509c7d1ab833f
Decheng <chinadecheng@gmail.com>
2019-04-12 14:03:51 +1000

    import antd css

--- src/components/GeneralTasksPage.js
+++ src/components/GeneralTasksPage.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import {Tabs} from 'antd';
-import Tasks from '../elements/Tasks'
+import { Tabs } from 'antd';
+import Tasks from '../elements/Tasks';
 const TabPane = Tabs.TabPane;
-class GenearlTasks extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            
-        }
-    }
+class GenearlTasks extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-    render(){
-        return (
-            <div>
-            <h2>Genearl Tasks</h2>
-            <Tabs defaultActiveKey="1">
-                <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
-                <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
-                <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
-            </Tabs>
-            </div>)
-    }
+	render() {
+		return (
+			<div>
+				<h2>Genearl Tasks</h2>
+				<Tabs defaultActiveKey='1'>
+					<TabPane tab='Tab 1' key='1'>
+						Content of Tab Pane 1
+					</TabPane>
+					<TabPane tab='Tab 2' key='2'>
+						Content of Tab Pane 2
+					</TabPane>
+					<TabPane tab='Tab 3' key='3'>
+						Content of Tab Pane 3
+					</TabPane>
+				</Tabs>
+			</div>
+		);
+	}
 }
 
-
-export default GenearlTasks
+export default GenearlTasks;
